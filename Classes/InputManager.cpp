fc727d1af1986bfc8685fb40a34a346d775079ba
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-18 13:49:30 +0300

    Program IM states

--- Classes/InputManager.cpp
+++ Classes/InputManager.cpp
@@ -1,9 +1,43 @@
 #include "InputManager.h"
 
+InputManager::InputManager()
+	:_left(false)
+	,_right(false)
+	,_jump(false)
+{}
+
 void InputManager::KeyPressed(cocos2d::EventKeyboard::KeyCode keyCode)
 {
+	switch (keyCode)
+	{
+	case LEFT:
+		_left = true;
+		break;
+
+	case RIGHT:
+		_right = true;
+		break;
+
+	case JUMP:
+		_jump = true;
+		break;
+	}
 }
 
 void InputManager::KeyReleased(cocos2d::EventKeyboard::KeyCode keyCode)
 {
+	switch (keyCode)
+	{
+	case LEFT:
+		_left = false;
+		break;
+
+	case RIGHT:
+		_right = false;
+		break;
+
+	case JUMP:
+		_jump = false;
+		break;
+	}
 }
--- Classes/InputManager.h
+++ Classes/InputManager.h
@@ -6,6 +6,14 @@ class InputManager : public cocos2d::Node
 {
 public:
 	CREATE_FUNC(InputManager);
+	InputManager();
 	void KeyPressed(cocos2d::EventKeyboard::KeyCode keyCode);
 	void KeyReleased(cocos2d::EventKeyboard::KeyCode keyCode);
+
+private:
+	static const cocos2d::EventKeyboard::KeyCode LEFT = cocos2d::EventKeyboard::KeyCode::KEY_L;
+	static const cocos2d::EventKeyboard::KeyCode RIGHT = cocos2d::EventKeyboard::KeyCode::KEY_R;
+	static const cocos2d::EventKeyboard::KeyCode JUMP = cocos2d::EventKeyboard::KeyCode::KEY_U;
+
+	bool _left, _right, _jump;
 };
