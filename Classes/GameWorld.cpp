5bf1470b1a9284215f9a69807fc74eec2d46a089
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-30 21:35:05 +0300

    Fix GameWorld class

--- Classes/GameWorld.cpp
+++ Classes/GameWorld.cpp
@@ -7,8 +7,8 @@ using namespace json11;
 
 GameWorld* GameWorld::create(PhysicsEngine *physEngine, std::string const& img, Json const& json)
 {
-	GameWorld *pRet = new (std::nothrow) GameWorld(physEngine);
-	if (pRet && pRet->init(img, json))
+	GameWorld *pRet = new (std::nothrow) GameWorld();
+	if (pRet && pRet->init(physEngine, img, json))
 	{
 		pRet->autorelease();
 	}
@@ -19,43 +19,38 @@ GameWorld* GameWorld::create(PhysicsEngine *physEngine, std::string const& img, 
 	return pRet;
 }
 
-GameWorld::GameWorld(PhysicsEngine *physEngine)
-	: m_physEngine(physEngine)
-{}
-
-bool GameWorld::init(std::string const& img, Json const& json)
+bool GameWorld::init(PhysicsEngine *physEngine, std::string const& img, Json const& json)
 {
 	if (!Sprite::initWithFile(img))
 		return false;
 
-	auto blockArray = json.array_items();
-	for (auto &block : blockArray)
+	for (auto &block : json.array_items())
 	{
 		const Vec2 leftUp(block["left"].number_value(), block["top"].number_value());
 		const Vec2 rightDown(block["right"].number_value(), block["bottom"].number_value());
 		const Size size(rightDown.x - leftUp.x, rightDown.y - leftUp.y);
-		AddRectBlock(Rect(Vec2(leftUp.x, getContentSize().height - leftUp.y - size.height), size));
+		AddRectBlock(Rect(Vec2(leftUp.x, getContentSize().height - leftUp.y - size.height), size), physEngine);
 	}
 
 	setAnchorPoint(Vec2());
 	return true;
 }
 
-void GameWorld::AddRectBlock(Rect const& rect)
+void GameWorld::AddRectBlock(Rect const& rect, PhysicsEngine *physEngine)
 {
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_staticBody;
-	bodyDef.position.Set(rect.getMidX() / m_physEngine->getPtmRatio(),
-		rect.getMidY() / m_physEngine->getPtmRatio());
+	bodyDef.position.Set(rect.getMidX() / physEngine->getPtmRatio(),
+		rect.getMidY() / physEngine->getPtmRatio());
 
 	b2PolygonShape rectShape;
-	rectShape.SetAsBox(rect.size.width / 2 / m_physEngine->getPtmRatio(),
-		rect.size.height / 2 / m_physEngine->getPtmRatio());
+	rectShape.SetAsBox(rect.size.width / 2 / physEngine->getPtmRatio(),
+		rect.size.height / 2 / physEngine->getPtmRatio());
 
 	b2FixtureDef fixtureDef;
 	fixtureDef.shape = &rectShape;
 
-	auto body = m_physEngine->createBody(bodyDef);
+	auto body = physEngine->createBody(bodyDef);
 	CCASSERT(body, "Can't create body");
 	body->CreateFixture(&fixtureDef);
 }
--- Classes/GameWorld.h
+++ Classes/GameWorld.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "cocos2d.h"
-#include "ObjectKeeper.h"
 
 class PhysicsEngine;
 namespace json11
@@ -13,10 +12,8 @@ class GameWorld : public cocos2d::Sprite
 {
 public:
 	static GameWorld* create(PhysicsEngine *physEngine, std::string const& img, json11::Json const& json);
-	GameWorld(PhysicsEngine *physEngine);
-	bool init(std::string const& img, json11::Json const& json);
+	bool init(PhysicsEngine *physEngine, std::string const& img, json11::Json const& json);
 
 private:
-	void AddRectBlock(cocos2d::Rect const& rect);
-	ObjectKeeper<PhysicsEngine> m_physEngine;
+	void AddRectBlock(cocos2d::Rect const& rect, PhysicsEngine *physEngine);
 };
