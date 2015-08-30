8b2c4185ea85a3033cc67ba0c32bb74c94e66dd1
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-30 21:16:56 +0300

    Fix Bonuses class

--- Classes/Bonus/Bonuses.cpp
+++ Classes/Bonus/Bonuses.cpp
@@ -8,8 +8,8 @@ using namespace json11;
 
 Bonuses* Bonuses::create(PhysicsEngine *physEngine, Json const& json, int mapHeight)
 {
-	Bonuses *pRet = new (std::nothrow) Bonuses(physEngine);
-	if (pRet && pRet->init(json, mapHeight))
+	Bonuses *pRet = new (std::nothrow) Bonuses();
+	if (pRet && pRet->init(physEngine, json, mapHeight))
 	{
 		pRet->autorelease();
 	}
@@ -20,34 +20,21 @@ Bonuses* Bonuses::create(PhysicsEngine *physEngine, Json const& json, int mapHei
 	return pRet;
 }
 
-Bonuses::Bonuses(PhysicsEngine *physEngine)
-	: m_physEngine(physEngine)
-{}
-
-bool Bonuses::init(Json const& json, int mapHeight)
+bool Bonuses::init(PhysicsEngine *physEngine, Json const& json, int mapHeight)
 {
 	if (!SpriteBatchNode::initWithFile("gfx/bonus_atlas.png"))
 		return false;
 
-	auto bonusArray = json.array_items();
-	for (auto &bonus : bonusArray)
+	for (auto &bonus : json.array_items())
 	{
 		Vec2 origin(JsonUtils::ParseVec2(bonus["origin"]));
 		origin.y = mapHeight - origin.y;
 
-		if (!AddBonus(JsonUtils::ParseRect(bonus["imgRect"]), origin))
+		auto bonusSprite = Bonus::create(getTexture(), JsonUtils::ParseRect(bonus["imgRect"]), origin, physEngine);
+		if (!bonusSprite)
 			return false;
+		addChild(bonusSprite);
 	}
-
-	return true;
-}
-
-bool Bonuses::AddBonus(Rect const& imageRect, Vec2 const& origin)
-{
-	auto bonus = Bonus::create(getTexture(), imageRect, origin, m_physEngine);
-	if (!bonus)
-		return false;
-	addChild(bonus);
 
 	return true;
 }
--- Classes/Bonus/Bonuses.h
+++ Classes/Bonus/Bonuses.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "cocos2d.h"
-#include "ObjectKeeper.h"
 
 class PhysicsEngine;
 namespace json11
@@ -13,10 +12,5 @@ class Bonuses : public cocos2d::SpriteBatchNode
 {
 public:
 	static Bonuses* create(PhysicsEngine *physEngine, json11::Json const& json, int mapHeight);
-	Bonuses(PhysicsEngine *physEngine);
-	bool init(json11::Json const& json, int mapHeight);
-
-private:
-	bool AddBonus(cocos2d::Rect const& imageRect, cocos2d::Vec2 const& origin);
-	ObjectKeeper<PhysicsEngine> m_physEngine;
+	bool init(PhysicsEngine *physEngine, json11::Json const& json, int mapHeight);
 };
